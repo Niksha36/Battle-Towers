29fc6d5676ab44dc1bb1ad17d4defbb27b02efdd
eMD0gG <147574275+eMD0gG@users.noreply.github.com>
2024-11-30 13:52:47 +1000

    Revert "Merge branch 'master' of https://github.com/Niksha36/fefu_survival"
    This reverts commit 80aab42122f2d8a44ba46996fec9dd372e692d50, reversing
    changes made to 7e83fc6ddccedb46411a2914b9c8995e5cc3e87b.

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -1,15 +1,12 @@
 import game from '../components/Game.vue'
 import register from '../components/RegistrationForm.vue'
 import login from '../components/LoginForm.vue'
-import menu from '../components/GameMenu.vue'
-import rating from '../components/RatingsComponent.vue'
+
 const routes = [
     {path: '/', redirect: () => '/game'},
     {path: '/game', component: game},
     {path: '/register', component: register},
     {path: '/login', component: login},
-    {path: '/menu', component: menu},
-    {path: '/rating', component: rating}
 ]
 
 export default routes
