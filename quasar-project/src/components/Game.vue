423ac25517789ea0e25c46a265af69cb086196a3
Nikit Shuro <i.niksha36@gmail.com>
2024-12-02 18:54:49 +1000

    login, registration, menu, rating ui has been updated

--- quasar-project/src/components/Game.vue
+++ quasar-project/src/components/Game.vue
@@ -21,7 +21,7 @@ export default {
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: {y: -1000},
+                    gravity: {y: 7000},
                     debug: false
                 }
             },
---
+++ quasar-project/src/components/SettingsScreen.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['close']);
+const isVisible = ref(false);
+
+function openSettings() {
+    isVisible.value = true;
+}
+
+function closeSettings() {
+    isVisible.value = false;
+    emit('close');
+}
+</script>
+
+<template>
+    <div v-if="isVisible" class="background-wrapper">
+        <div class="leave-the-game-dialog">
+            <h2>Выйти из игры</h2>
+            <span>Вы уверены что хотите покинуть игру? Ваш прогресс будет утерен безвозвратно.</span>
+            <div class="buttons-wrapper">
+                <button @click="openSettings" class="submit-button">Да</button>
+                <button @click="closeSettings" class="cancel-button">Нет</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.background-wrapper {
+    z-index: 10000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.leave-the-game-dialog {
+}
+.button-wrapper {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
