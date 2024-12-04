e2a6b71a03f5f27be8a203a8dfb4f7f1fd73f3da
zxcpisya <mamin.papa0@gmail.com>
2024-12-04 23:07:58 +1000

    fix menu logout

--- vueApp/user/serializers.py
+++ vueApp/user/serializers.py
@@ -13,6 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        return user
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
